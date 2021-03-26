5da2c6652da578dc9f03f57efd10c2c6a5589741
BlackElias <valienne.elias@outlook.com>
2021-03-26 10:53:01 +0100

    added front-end design

--- index.php
+++ index.php
@@ -11,15 +11,15 @@ $conn = Db::getConnection();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <link rel="stylesheet" href="https://use.typekit.net/zbb0stp.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Logo</a>
+    <img class="logo" src="img/gg-logo.png" alt="logo">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -27,7 +27,7 @@ $conn = Db::getConnection();
       <div class="navbar-nav">
         <a class="nav-link active" aria-current="page" href="#">Contact</a>
         <a class="nav-link" href="#">Login</a>
-        <a class="nav-link" href="#">About Us</a>
+        <a class="nav-link" href="#">About </a>
        
       </div>
     </div>
@@ -36,14 +36,8 @@ $conn = Db::getConnection();
 <body>
 <div class="form wrapper">
 
-<label  class="formTitleEmail"><h2>Email:</h2> </label>
+<label  class="formTitleEmail"><h1>Login</h1> </label>
 <input type="email" class="formEmail"  placeholder="name@example.com">
-
-<label  class="formTitleUsername"><h2>Username:</h2></label>
-<input type="name" class="formUsername"  placeholder="Username">
-
-<label  class="formTitlePassword"><h2>Password:</h2></label>
-
 <input type="password" class="formPassword"  placeholder="password">
 <div class="sign_up">
 <p>Nog geen account? <a href="sign_up.php">Maak er één aan</a></p>
@@ -53,11 +47,11 @@ $conn = Db::getConnection();
 </div>
    <div class="hero_bg"></div>
    
-<footer class="footer bg-light text-center text-lg-start">
+<footer class="footer  text-center text-lg-start">
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © 2020 Copyright:
-    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    <a class="text-dark" href="https://mdbootstrap.com/">Aaron, Tommy, Elias</a>
   </div>
   <!-- Copyright -->
 </footer>
