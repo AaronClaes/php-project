3e7715772cc22c3b78b59327ff66fbd8232ea849
BlackElias <valienne.elias@outlook.com>
2021-04-12 12:57:53 +0200

    added logo to signup

---
+++ signup.php
@@ -0,0 +1,104 @@
+<?php
+
+include_once("bootstrap.php");
+
+if (!empty($_POST)) {
+  try {
+    $user = new User();
+    $user->setEmail($_POST["email"]);
+    $user->setFirstname($_POST["firstName"]);
+    $user->setLastname($_POST["lastName"]);
+    $user->setUsername($_POST["username"]);
+    $user->setPassword($_POST["password"]);
+    $user->save();
+    session_start();
+    $_SESSION["username"] = $user->getUsername();
+    header("Location: index.php");
+  } catch (\Throwable $th) {
+    $error = $th->getMessage();
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+  <img class="logo" src="img/gg-logo.png" alt="logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Contact</a>
+        <a class="nav-link" href="#">Login</a>
+        <a class="nav-link" href="#">About Us</a>
+
+      </div>
+    </div>
+  </div>
+</nav>
+
+<body>
+  <?php if (isset($error)) : ?>
+    <div class="error">
+      <h3><?php echo $error ?></h3>
+    </div>
+  <?php endif; ?>
+  <form action="" method="POST">
+    <div class="form wrapper">
+
+      <label class="formTitleEmail">
+        <h2>Email:</h2>
+      </label>
+      <input type="email" class="formEmail" name="email" placeholder="name@example.com">
+
+      <label class="formTitleFirstName">
+        <h2>First Name:</h2>
+      </label>
+      <input type="text" class="formFirstName" name="firstName" placeholder="First Name">
+
+      <label class="formTitleLastName">
+        <h2>Last Name:</h2>
+      </label>
+      <input type="text" class="formLastName" name="lastName" placeholder="Last Name">
+
+
+      <label class="formTitleUsername">
+        <h2>Username:</h2>
+      </label>
+      <input type="name" class="formUsername" name="username" placeholder="Username">
+
+      <label class="formTitlePassword">
+        <h2>Password:</h2>
+      </label>
+      <input type="password" class="formPassword" name="password" placeholder="password">
+
+      <button type="submit" class="btn btn-primary">Sign up</button>
+
+    </div>
+  </form>
+  <div class="hero_bg2"></div>
+
+  <footer class="footer bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:
+      <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+</body>
+
+</html>
