f3c5dca105353119f4356780285527d9d0386184
Aaron Claes <aaronclaes22@gmail.com>
2021-05-13 15:02:32 +0200

    feat: add search options + styling

--- header.inc.php
+++ header.inc.php
@@ -1,27 +1,18 @@
-<?php
-if (!empty($_GET['search'])) {
-    try {
-        $user = new User;
-        $searchresult = $_GET['search'];
-        $users = $user->searchusers($searchresult, $tags);
-    } catch (\Throwable $th) {
-        $error = $th->getMessage();
-    }
-}
-?>
-
 <nav class="navbar navbar-expand navbar-dark bg-dark fixed-top" aria-label="Second navbar example">
-    <div class="container-fluid">
+    <div class="container-fluid nav-container">
         <a class="navbar-brand" href="index.php"><img class="logo" src="img/gg-logo.png" alt=""></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <label>Search</label>
-        <form action="search.php" method="GET">
-            <input type="text" placeholder="Type the name here" name="search">&nbsp;
-            <input type="submit" value="Search" name="btn" class="btn btn-sm btn-primary">
+        <form class="searchbar" action="search.php" method="GET">
+            <div class="input-group" id="search">
+                <select class="form-select form-border" id="search-select" aria-label="Floating label select example">
+                    <option selected>Content</option>
+                    <option value="1">Tags</option>
+                    <option value="2">Location</option>
+                    <option value="3">Users</option>
+                </select>
+                <input type=" text" class="form-control form-border" id="searchbar" placeholder="Type the name here" name="search">&nbsp;
+            </div>
         </form>
-        <div class="collapse navbar-collapse navbar-nav me-auto">
+        <div class="navbar-nav">
             <a href="logout.php" class="btn nav-btn"> Logout </a>
             <!-- <form data-np-checked="1">
                 <input class="form-control" type="text" placeholder="Search" aria-label="Search" data-np-checked="1">
